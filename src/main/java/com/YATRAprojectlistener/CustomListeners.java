7d1cbde8e099a5aebe9d9cac34922be32ab18b79
javedchowdhury001 <javedchowdhury001@gmail.com>
2020-08-05 01:00:31 -0400

    my second commit

---
+++ src/main/java/com/YATRAprojectlistener/CustomListeners.java
@@ -0,0 +1,105 @@
+package com.YATRAprojectlistener;
+
+import java.util.Date;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+
+public class CustomListeners implements ITestListener,ISuiteListener {
+
+	static Date d = new Date();
+	static String fileName = "Extent_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
+	static String messageBody;
+	private static ExtentReports extent = ExtentManager.createInstance(System.getProperty("user.dir")+"\\Reports\\"+fileName);
+	public static ThreadLocal<ExtentTest> testReport = new ThreadLocal<ExtentTest>();
+	
+	public void onTestStart(ITestResult result) {
+		
+		System.out.println( "*************** "+result.getName() +" is Staring **************************");
+	
+		ExtentTest test = extent.createTest("TestCase : "+result.getMethod().getMethodName());
+        testReport.set(test);
+        
+
+	}
+
+	public void onTestSuccess(ITestResult result) {
+
+		System.out.println("*************** "+ result.getName() +" is Passed ***************************");
+		
+		String methodName=result.getMethod().getMethodName();
+		String logText="<b>"+"TEST CASE:- "+ methodName.toUpperCase()+ " PASSED"+"</b>";		
+		Markup m=MarkupHelper.createLabel(logText, ExtentColor.GREEN);
+		testReport.get().pass(m);
+		
+
+	}
+
+	public void onTestFailure(ITestResult result) {
+		System.out.println("***************** "+ result.getName() +" is Failed **********************");
+
+		
+		
+		}
+		
+		
+	
+
+	public void onTestSkipped(ITestResult result) {
+		
+		System.out.println("***************** "+ result.getName() +" is Skipped ************************");
+		
+		String methodName=result.getMethod().getMethodName();
+		String logText="<b>"+"Test Case:- "+ methodName+ " Skipped"+"</b>";		
+		Markup m=MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
+		testReport.get().skip(m);
+
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onStart(ITestContext context) {
+
+		
+
+	}
+
+	public void onFinish(ISuite arg0) {
+		
+		
+
+		
+	}
+
+	public void onStart(ISuite arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+
+
+		if (extent != null) {
+
+			extent.flush();
+		}
+		
+	}
+
+
+}
